a40101d60c2d8ff6208ec324f9d4ad9414007950
mrasoahaingo <michael.rasoahaingo@gmail.com>
2017-10-11 01:53:25 +0200

    design & animation

--- client/views/PlayPage/Camera.js
+++ client/views/PlayPage/Camera.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import loadScript from 'simple-load-script';
-import skullImage from '../../images/skull.png';
+import skullImage from './skull.png';
+import spriteImage from './sprite.png';
 
 class Camera extends React.Component {
   state = {
@@ -8,24 +9,29 @@ class Camera extends React.Component {
     y: 0,
     width: 0,
     height: 0,
+    ticks: 0,
     gifSrc: null,
   };
-  componentDidMount() {
-    if (this.video && typeof navigator !== 'undefined') {
-      navigator.getUserMedia(
-        {
-          video: true,
-          width: 320,
-          height: 240,
+
+  componentWillMount() {
+    this.setState(() => ({
+      width: Math.min(window.innerWidth, 414),
+      height: Math.min(window.innerWidth, 736),
+    }));
+  }
+
+  async componentDidMount() {
+    const { width, height } = this.state;
+    if (this.video && typeof navigator !== 'undefined' && typeof navigator.mediaDevices !== 'undefined') {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: {
+          width,
+          height,
         },
-        stream => {
-          this.video.srcObject = stream;
-          this.initTracker();
-        },
-        () => {
-          // no support
-        },
-      );
+      });
+      this.stream = stream;
+      this.video.srcObject = stream;
+      this.initTracker();
     }
   }
 
@@ -33,6 +39,7 @@ class Camera extends React.Component {
   canvas = null;
   gif = null;
   tracker = null;
+  stream = null;
 
   initTracker = async () => {
     const trackerFile = await import('file-loader!../../../libs/clmtrackr.js' /* webpackChunkName: 'clmtrackr' */); //eslint-disable-line
@@ -43,67 +50,83 @@ class Camera extends React.Component {
   };
 
   startRecording = async () => {
+    const { width, height } = this.state;
     const canvas = this.canvas;
     const context = canvas.getContext('2d');
     const interval = 100;
-    let ticks = 3500 / interval;
+    let frame = 0;
+    let ticks = 35000 / interval;
     let lastFrameTime = Date.now();
     let timer = null;
 
     const GIF = await import('gif.js.optimized' /* webpackChunkName: 'gifjs' */);
     const gifWorkerFile = await import('file-loader!../../../libs/gif.worker.js' /* webpackChunkName: 'gifworker' */); //eslint-disable-line
 
-    const skull = new Image(178, 178);
+    const skull = new Image(200, 200);
     skull.src = skullImage;
     skull.crossOrigin = 'anonymous';
+
+    const sprite = new Image(480, 384);
+    sprite.src = spriteImage;
+    sprite.crossOrigin = 'anonymous';
 
     this.gif = new GIF({
       workers: 16,
       quality: 12,
       workerScript: gifWorkerFile,
-      width: canvas.width,
-      height: canvas.height,
+      width,
+      height,
     });
 
     const drawLoop = () => {
       ticks -= 1;
+      frame = (frame + 1) % 10;
+      
+      this.setState(() => ({
+        ticks,
+      }));
 
       if (ticks <= 0) {
         clearInterval(timer);
         return;
       }
 
-      console.log('ticks', ticks);
-
       const positions = this.tracker.getCurrentPosition();
       const leftEye = positions[27];
       const rightEye = positions[32];
-      // const [ rightX, rightY ] = positions[32];
-      if (leftEye) {
-        context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        const angle = Math.atan((rightEye[1] - leftEye[1]) / (rightEye[0] - leftEye[0]));
-        const distance = rightEye[0] - leftEye[0];
-        const ratio = distance / 40;
-        const [offsetX, offsetY] = [89 + leftEye[0] / 2, 30 + leftEye[1] / 2];
-        // player
-        context.drawImage(this.video, 0, 0, canvas.width, canvas.height);
+      context.clearRect(0, 0, width, height);
+      if (leftEye && rightEye) {
+        const x = rightEye[0] - leftEye[0];
+        const y = rightEye[1] - leftEye[1];
+        // const angle = Math.atan(y / x);
+        const distance = Math.sqrt(x * x + y * y);
+        const ratio = distance / 60; // 80 = eyes skull distance
+        const sized = 200 * ratio;
+        const offsetX = distance / 2;
+        const offsetY = 10 * ratio;
+        const X = offsetX + leftEye[0] - sized / 2;
+        const Y = offsetY + leftEye[1] - sized / 2;
+        // const originX = X + sized / 2;
+        // const originY = Y + sized / 2;
+        context.drawImage(this.video, 0, 0, width, height);
         // start
-        context.save();
-        context.translate(offsetX, offsetY);
-        context.rotate(angle);
-        context.scale(ratio, ratio);
-        context.translate(-offsetX, -offsetY);
-        // skull
-        context.drawImage(skull, leftEye[0] - 69, leftEye[1] - 72);
+        // context.save();
+        // context.translate(-originX, -originY);
+        // context.rotate(angle);
+        // context.translate(originX, originY);
+        context.drawImage(skull, X, Y, sized, sized);
+        context.drawImage(sprite, frame * 100, 0, 100, 100, 0, 0, 100, 100);
         // end
-        context.restore();
+        // context.restore();
       }
+
+      const now = Date.now();
 
       this.gif.addFrame(context, {
         copy: true,
-        delay: Date.now() - lastFrameTime,
+        delay: now - lastFrameTime,
       });
-      lastFrameTime = Date.now();
+      lastFrameTime = now;
     };
 
     timer = setInterval(drawLoop, interval);
@@ -137,15 +160,16 @@ class Camera extends React.Component {
   };
 
   render() {
-    const { gifSrc } = this.state;
+    const { gifSrc, width, height, ticks } = this.state;
     return (
       <div className="camera">
         <video
           ref={el => {
             this.video = el;
           }}
-          width="320"
-          height="240"
+          className="camera__media"
+          height={height}
+          width={width}
           preload
           autoPlay
           playsInline
@@ -156,17 +180,16 @@ class Camera extends React.Component {
           <track kind="description" src="" />
         </video>
         <canvas
-          key="canvas"
           ref={el => {
             this.canvas = el;
           }}
-          id="canvas"
-          width="320"
-          height="240"
+          className="camera__preview"
+          height={height}
+          width={width}
         />
-        <div key="buttons">
-          <button onClick={this.startRecording}>start recording</button>
-          <button onClick={this.generateGif}>generate gif</button>
+        <div className="camera__buttons">
+          <a onClick={this.startRecording} className="camera__button">{ticks}</a>
+          <a onClick={this.generateGif} className="camera__button">generate gif</a>
         </div>
         {gifSrc && <img key="gif" src={gifSrc} alt="gif" />}
       </div>
